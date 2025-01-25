9efa6f6905a40a2e6c0bc2575c568f778faa8776
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-01-25 18:17:42 -0500

    imported style sheets

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/index.css';
+import './styles/aboutme.module.css';
+import './styles/header.module.css';
+import './styles/footer.module.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
