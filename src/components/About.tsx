2aea799e404fa1f4df5561847aa4f8c0d0359bd2
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-01-25 02:50:21 -0500

    migrated old html into react; css stylings included; modified website

---
+++ src/components/About.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const AboutMe = () => (
+  <section className="profile">
+    <h3>About Me</h3>
+    <div>
+      <h4>Quote:</h4>
+      <p>"Wave at the vanishing of yesterdays."</p>
+    </div>
+    <div>
+      <h4>Colors:</h4>
+      <ul className="colors">
+        <li className="black">Black</li>
+        <li className="hotpink">Hot Pink</li>
+        <li className="babyblue">Baby Blue</li>
+        <li className="lavender">Lavender</li>
+      </ul>
+    </div>
+    <div>
+      <h4>Video Games:</h4>
+      <p>Bayonetta, Child of Light, Life is Strange, Spiritfarer</p>
+    </div>
+    <div>
+      <h4>Anime:</h4>
+      <p>Bayonetta: Bloody Fate, Hell Girl (地獄少女), Sailor Moon Crystal</p>
+    </div>
+    <div>
+      <h4>TV Shows:</h4>
+      <p>The Office, Chucky, Spongebob Squarepants</p>
+    </div>
+    <div>
+      <h4>Films:</h4>
+      <p>A Dog's Journey, Godzilla: King of the Monsters, Storks</p>
+    </div>
+    <div>
+      <h4>Music:</h4>
+      <p>2NE1, BoA, f(x), Lindsey Stirling, Rina Sawayama, Taylor Swift</p>
+    </div>
+    <div>
+      <h4>Special Person:</h4>
+      <p>Alfie</p>
+    </div>
+  </section>
+);
+
+export default AboutMe;
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => (
+  <header>
+    <h1>Hello, nice to meet you!</h1>
+    <nav className="nav">
+      {/* <ul className="links">
+        <li>
+          <a href="https://github.com/sabrinaira" target="_blank" rel="noopener noreferrer">Return to Github</a>
+        </li>
+      </ul> */}
+    </nav>
+  </header>
+);
+
+export default Header;
